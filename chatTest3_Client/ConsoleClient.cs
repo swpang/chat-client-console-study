f0d32b83a25c2eaefc633fa20767e53a29ee4028
Seungwon Pang <legomaniac1005@gmail.com>
2021-09-19 15:10:48 +0900

    created StaticDefine

--- chatTest3_Client/ConsoleClient.cs
+++ chatTest3_Client/ConsoleClient.cs
@@ -11,6 +11,48 @@ namespace chatTest3_Client
 {
     class ConsoleClient
     {
-        TcpClient
+        TcpClient client = null;
+        Thread receiveMessageThread = null;
+        ConcurrentBag<string> sendMessageListToView = null;
+        ConcurrentBag<string> receiveMessageListToView = null;
+        private string name = null;
+
+        public void Run()
+        {
+            sendMessageListToView = new ConcurrentBag<string>();
+            receiveMessageListToView = new ConcurrentBag<string>();
+            receiveMessageThread = new Thread(receiveMessage);
+
+            while (true)
+            {
+                Console.WriteLine("=============Client=============");
+                Console.WriteLine("1. 서버 연결");
+                Console.WriteLine("2. Message 보내기");
+                Console.WriteLine("3. 보낸 Message 확인");
+                Console.WriteLine("4. 받은 Message 확인");
+                Console.WriteLine("0. 종료");
+                Console.WriteLine("================================");
+
+                string key = Console.ReadLine();
+                int order = 0;
+
+                if (int.TryParse(key, out order))
+                {
+                    switch (order)
+                    {
+                        case StaticDefine.CONNECT:
+                            if (client != null)
+                            {
+                                Console.WriteLine("Already Connected");
+                                Console.ReadKey();
+                            }
+                            else
+                                Connect();
+                            break;
+                        case Static
+                    }
+                }
+            }
+        }
     }
 }
---
+++ chatTest3_Client/StaticDefine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chatTest3_Client
+{
+    class StaticDefine
+    {
+        public const int CONNECT = 1;
+        public const int SEND_MESSAGE = 2;
+        public const int SEND_MSG_VIEW = 3;
+        public const int RECEIVE_MSG_VIEW = 4;
+        public const int EXIT = 0;
+    }
+}
