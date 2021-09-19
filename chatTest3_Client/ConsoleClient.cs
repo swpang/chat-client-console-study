3778d403bcf969f2df13c29758f26f0fbd1ea679
Seungwon Pang <legomaniac1005@gmail.com>
2021-09-19 13:59:43 +0900

    Add project files.

---
+++ chatTest3_Client/ConsoleClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace chatTest3_Client
+{
+    class ConsoleClient
+    {
+        TcpClient
+    }
+}
---
+++ chatTest3_Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace chatTest3_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleClient a = new ConsoleClient();
+            a.Run();
+        }
+    }
+}
